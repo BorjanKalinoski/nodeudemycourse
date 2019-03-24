40cc2ee99ad3e05a919125079e6c18dcb5236c5a
drsmrtnik <boki_borjan@hotmail.com>
2019-03-24 17:55:34 +0100

    trying something new

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const forecast = ({latitude, longitude}, callback) => {
     const url = `https://api.darksky.net/forecast/27f351d61f5eccd267bd816dcd6af2cc/${latitude},${longitude}`;
     request({url: url,json:true}, (error, data) => {
         if (error) {
-            callback(error, undefined);
+            callback(error,undefined);
         } else if(data.code===400){
             callback('The given location is invalid', undefined);
         }else{
